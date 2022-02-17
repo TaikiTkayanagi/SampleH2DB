4791888c2917be67b698ccc86fd7311a975802dd
TaikiTkayanagi <ikiat4@gmail.com>
2022-02-17 23:24:49 +0900

    sqlファイルからのsqlの実行

---
+++ app/bin/main/create.sql
@@ -0,0 +1,21 @@
+create table
+if not exists
+users (
+    firstname varchar(30),
+    lastname varchar(30)
+);
+
+insert into
+users values (
+    'f1','p1'
+);
+
+insert into
+users values (
+    'f2','p1'
+);
+
+insert into
+users values (
+    'f3','p1'
+);
--- app/src/main/java/h2db/data/H2DBSample.java
+++ app/src/main/java/h2db/data/H2DBSample.java
@@ -1,47 +1,99 @@
 package h2db.data;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import org.h2.Driver;
 import org.h2.tools.Server;
-import org.h2.util.NetUtils;
 
 public class H2DBSample {
-    Server server;
+    public Server webServer;
+    public Server tcpServer;
+    public String webPort;
+    public String tcpPort;
 
-    public H2DBSample() {
+    public H2DBSample(String webPort, String tcpPort) {
+        this.webPort = webPort;
+        this.tcpPort = tcpPort;
+
         try {
-            //this.server = Server.createTcpServer("-tcp", "-tcpAllowOthers").start();
-            this.server = Server.createWebServer("-web", "-webAllowOthers").start();
-            String localAddress = NetUtils.getLocalAddress();
-            System.out.println("アドレス: " + localAddress);
-            System.out.println("サーバポート: " + this.server.getPort());
-            System.out.println("URL: " + this.server.getURL());
+            this.tcpServer = Server.createTcpServer("-tcp", "-ifNotExists", "-tcpPort", this.tcpPort).start();
+            this.webServer = Server.createWebServer("-web", "-ifNotExists", "-webPort", this.webPort).start();
+            System.out.println("URL(web): " + this.webServer.getURL());
+            System.out.println("URL(tcp): " + this.tcpServer.getURL());
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    private Connection connect() throws SQLException {
-        System.out.println("jdbc:h2:" + this.server.getURL() +
-                "/mem:test;DB_CLOSE_DELAY=-1");
-        Connection connection = DriverManager.getConnection("jdbc:h2:" + this.server.getURL() +
-                "/mem:test;DB_CLOSE_DELAY=-1", "sa", "");
+    public H2DBSample(){
+        this.webPort = "8088";
+        this.tcpPort = "9000";
 
-        return connection;
+        try {
+            this.tcpServer = Server.createTcpServer("-tcp", "-ifNotExists", "-tcpPort", this.tcpPort).start();
+            this.webServer = Server.createWebServer("-web", "-ifNotExists", "-webPort", this.webPort).start();
+            System.out.println("URL(web): " + this.webServer.getURL());
+            System.out.println("URL(tcp): " + this.tcpServer.getURL());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void initExe(){
-        try (Connection connection = this.connect()) {
-            Statement stmt = this.connect().createStatement();
-            stmt.executeUpdate("create table if not exists user (firstname varchar(30), lastname varchar(30))");
-            stmt.executeUpdate("insert into user values ('f1','p1')");
-            stmt.executeUpdate("insert into user values ('f2','p1')");
-            stmt.executeUpdate("insert into user values ('f3','p1')");
+        Driver.load();
+
+        try (Connection connection = DriverManager.getConnection("jdbc:h2:mem:test;", "sa", "")) {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(this.getCreateSql());
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private String getCreateSql() {
+        String path = "/create.sql";
+
+        try(InputStream is = getClass().getResourceAsStream(path)){
+
+            BufferedReader br = new BufferedReader((new InputStreamReader(is)));
+
+            String result = this.loadFill(br);
+
+            return result.toString();
+
+        } catch(IOException e){
+
+            e.printStackTrace();
+
+            return null;
+        }
+
+    }
+
+    private String loadFill(BufferedReader br){
+        boolean flag = false;
+        try {
+            flag = br.ready();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        StringBuilder sb = new StringBuilder();
+
+        while(flag){
+            try {sb.append(br.readLine() + " ");}
+            catch (IOException e) {e.printStackTrace();}
+            try {flag = br.ready();}
+            catch (IOException e) {e.printStackTrace();}
+        }
+        System.out.println("sql: " + sb.toString());
+
+        return sb.toString();
     }
 }
